778e2e55965a10e3232a3b63ed6a9625f009f6e0
lokeshvenkatesan <lokesh97cse@gmail.com>
2021-01-19 18:40:29 +0530

    Added navbar with bootstrap functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,24 +2,32 @@ import React from 'react';
 import PropTypes from 'prop-types';//impt
 
 
-const Header =(props)=>{
+const Header = props =>{
   const {branding} = props;
   return(
-    <div>
-      <h1 style={headingStyle}>
-      {branding}
-      </h1>
-    </div>
+   < nav className="navbar navbar-expand-sm navbar-dark bg-danger mb-3 py-0">
+     <div className="container">
+       <a href="/" className="navbar-brand">{branding}
+       </a>
+       <div>
+         <ul className="navbar-nav mr-auto">
+           <li className="nav-item">
+             <a href="/" className="nav-link">Home</a>
+           </li>
+         </ul>
+       </div>
+     </div>
+     </ nav>
   );
 };
 
 Header.defaultProps={
   branding: 'App'
-};
+}; 
 
-Header.propTypes={
+ Header.propTypes={
   branding: PropTypes.string.isRequired
-};
+}; 
 const headingStyle ={
   color:'purple',
   fontSize:'25px'
