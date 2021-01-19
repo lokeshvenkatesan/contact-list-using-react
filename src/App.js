69b5a4a19f374b2b8774fa6442e0cb3c4ebb9afe
lokeshvenkatesan <lokesh97cse@gmail.com>
2021-01-19 18:04:55 +0530

    installed and imported bootstrap.min.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import bootstrap/dist/css/bootstrap.min.css;
 
 export class App extends Component {
   render() {
