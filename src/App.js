fbc1eeea2843909809d8f48c552b6336fa80b590
lokeshvenkatesan <lokesh97cse@gmail.com>
2021-01-22 00:27:09 +0530

    Axios updated

--- src/App.js
+++ src/App.js
@@ -2,8 +2,43 @@ import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
 export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.FetchPosts = this.FetchPosts.bind(this);
+  
+    this.state = {
+       data:null
+    }
+  }
+
+  // In this way you can fetch data using API and display it to UI.
+  // Now you've to first complete the UI according to that website, like display table of posts, add new button, new post form in a modal, 
+  // Also design contact page and after all UI is done fetch these APIs and display data in 
+  
+// Actually am familiar with FUNC COMP, so this class componens create some problems like u can see below its not getting the function but yet we've defined it.. can you solve it
+
+// l
+  
+  FetchPosts = async ()=> {    // Here we use "async" keyword to change the type of function to ASYNCHRONOUS FUNCTION
+  await axios('https://jsonplaceholder.typicode.com/todos/1')
+  .then((response)=>{
+    console.log("THE DATA WE GOT: ",response.data);
+    this.setState({data:response.data})
+    this.forceUpdate()
+  })
+  .catch((error)=> console.log("OOPS! THERE'S SOME ERROR",error))
+
+}
+
+componentDidMount(){ //This is inbuit Lifecycle method of React CLass Components
+  //ComponentDidMount inbuilt method - when you want to run something as soon as the UI loads(mounts), you put that in this method. 
+  // Here as soon as the component is loaded on the browser, API is called and data is received
+  // now let us store the data in a variable so that we can display it on UI
+  this.FetchPosts();
+}
   render() {
     return (
       <div>
@@ -13,11 +48,27 @@ export class App extends Component {
           Email= "mohan@gmail.com"
           phone="9093939922" /> */}
         <Contact name= "Hello from Home"/>
+
+        <h1>User DATA Fetched</h1>
+        {
+          this.state.data!=null?
+          
+          <ul>
+          <li>{this.state.data.userId}</li>
+          <li>{this.state.data.completed}</li>
+          <li>{this.state.data.title}</li>
+          
+        </ul> : <ul>
+          <li>Nothing to show</li>
+          
+        </ul>
       
+    }
           {/* Email= "lokesh@gmail.com"
           phone="903232898"  */}
       
       {/* <Contact /> */}
+          
       </div>
     );
   }
--- src/api/jsonPlaceholder.js
+++ src/api/jsonPlaceholder.js
@@ -1,5 +1,25 @@
 import axios from "axios";
 
-export default axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com"
-});
+// export default axios.create({
+//     baseURL: "https://jsonplaceholder.typicode.com"
+// });
+
+// We create a function in which we'll Fetch the API and store the data(which came as response of that API) in a variable.
+ //We'll Call the function to run.  ?? Can you re-runn the react-We have App.js, Contact, Header files. How can I add this function
+//We can put this function anywhere We want like that one as of now. You understood?
+
+// ASYNCHRONOUS FUNCTION - It is a type of function which doesn't stop the program's flow.
+
+// Like when you fetch the api, it takes time(for eg.slow internet) to send request to server and get the response back to us(client).
+// So during this time your code shouldn't stop  rendering, so asynchronous menas run the function in background and let the code continue its running
+
+// "await" keyword is used infront of that code of line which is going to take time, i.e. our API request using axios. (This rough understanding is /// enough for a start)
+
+// Now axios takes in an address for which to request
+
+// AXIOS has 2 blocks of code, what to when request is SUCCESSFUL (then BLOCK) and when request FAILS (catch BLOCK)
+// So then block (as it is success) willl give you response(which contains our data) 
+
+// When it fails, it'll give ERROR(instead of data) so that we can see what's wrong and correct it, which we manage in CATCH block
+
+//SO NOW WE'LL DISPLAY DATA IN CONSOLE
